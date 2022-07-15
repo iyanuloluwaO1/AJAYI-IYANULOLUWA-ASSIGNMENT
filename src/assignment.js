f0f8410a8a2026ef0a1501e86706708fa5cec8e2
Iyanuloluwa  AJAYI <>
2022-07-15 00:01:55 +0000

    Completed Assignment

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,13 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum = 0;
+  for (let name of arrayOfNumbers) {
+    sum = sum + name;
+  } 
+  return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +32,15 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let count = 0;
+    for (let num of arrayOfNumbers){
+   if (num % 2 == 0){
+count++;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+}
+return count;
+}
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
